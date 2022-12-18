98d42bc44fc777ec392d0a8456b1e97c4a63d49d
Ej Sadiarin <ejsadiarin@gmail.com>
2022-12-18 21:03:41 +0800

    created CreateTable() method

---
+++ DatabaseAccess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.Data.Sqlite;
+
+namespace New_Coding_Tracker
+{
+    public class DatabaseAccess
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        
+        // Create Table
+        public static void CreateTable()
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText =
+                    @"CREATE TABLE IF NOT EXISTS codingtracker (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                        Date TEXT,
+                        Duration TEXT
+                    )";
+
+                tableCmd.ExecuteNonQuery();
+                
+                connection.Close();
+            }
+        }
+    }
+}
