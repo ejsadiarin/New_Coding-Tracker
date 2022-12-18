67e981e3c7f764be0dcc08891fcadeecb3e543c5
Ej Sadiarin <ejsadiarin@gmail.com>
2022-12-18 20:46:15 +0800

    added configuration manager nuget

---
+++ DatabaseCreation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace New_Coding_Tracker
+{
+    public class DatabaseCreation
+    {
+        string connectionString = ConfigurationManager.AppSettings.Get("ConnectionString");
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Coding_Tracker
+{
+    internal class Model
+    {
+        // used to interact with database and has business logic
+
+        // switch cases
+        bool notRunning = true;
+        
+        /*switch (!notRunning)
+            {
+
+            }*/
+    }
+}
+
